a6931f2e1ac193b40d1a60ab7cde46e1e6f1a5b0
patooromero <patriciooscarr@gmail.com>
2025-01-21 00:20:21 -0300

    cambios apk, ruta descarga v1

--- src/main.py
+++ src/main.py
@@ -3,18 +3,20 @@ from yt_dlp import YoutubeDL
 import requests
 from PIL import Image
 from io import BytesIO
+import os
 
 def main(page: ft.Page):
     # Configuración de la página
     page.title = "Descargador de Videos"
-    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
 
     # Elementos de la UI
     url_input = ft.TextField(label="Enlace del video", width=300)
     output_text = ft.Text(size=12)
     thumbnail_image = ft.Image(width=150, height=150)
     progress_bar = ft.ProgressBar(width=300, value=0)
+    download_path_text = ft.Text(size=12)  # Mostrar la ruta del archivo descargado
 
     # Botón de descarga
     download_button = ft.ElevatedButton("Descargar", disabled=True, width=150)
@@ -67,18 +69,25 @@ def main(page: ft.Page):
         download_button.bgcolor = ft.colors.LIGHT_BLUE
         page.update()
 
-        try:
+        try:   
+            if not os.path.exists("/storage/emulated/0/DCIM/DownFlet"):
+                os.makedirs("/storage/emulated/0/DCIM/DownFlet")
             ydl_opts = {
-                "outtmpl": "videos/%(title)s.%(ext)s",  # Carpeta de salida
+                #"outtmpl": "videos/%(title)s.%(ext)s",  # Carpeta de salida(pc)
+                #"outtmpl": "/storage/emulated/0/Download/%(title)s.%(ext)s", #op1
+                "outtmpl": "/storage/emulated/0/DCIM/DownFlet/%(title)s.%(ext)s", #op2
                 "format": "best",  # Mejor calidad disponible
                 "progress_hooks": [hook_progreso],  # Progreso
             }
             with YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
+                info = ydl.extract_info(url, download=True)
+                download_path_text.value = f"Archivo guardado en: videos/{info['title']}.{info['ext']}"
+                page.update()
 
             output_text.value = "¡Descarga completada!"
         except Exception as e:
             output_text.value = f"Error al descargar: {str(e)}"
+            download_path_text.value = ""
 
         # Restaurar el botón al estado inicial
         download_button.text = "Descargar"
@@ -108,6 +117,7 @@ def main(page: ft.Page):
         download_button,
         progress_bar,
         output_text,
+        download_path_text,  # Añadir la ruta del archivo al final
     )
 
 # Ejecutar la aplicación
